ea461fcc31d9b9210c32d2524cb7cb3be6a29587
AlaskanBeard <zack12179@gmail.com>
2018-10-10 22:45:42 -0800

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var express = require('express'),
     pg = require('pg'),
     app = express();
 
+// DB Connect String
+var connect = "postgres://recipe:Password1.@localhost/recipes";
+
 // Assign Dust Engine to .dust Files
 app.engine('dust', cons.dust);
 
@@ -20,32 +23,40 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-    // DB Connect String    
-const { Pool } = require('pg')
-var connectionString = 'postgresql://recipe:Password1.@localhost/recipes';
-const pool = new Pool({
-  connectionString: connectionString,
-})
-module.exports = {
-  query: (text, params, callback) => {
-    return pool.query(text, params, callback)
-  }
-}
+app.get ('/', function(req, res){
+    // PG Connect
+    var pool = new pg.pool();
 
-app.get('/', function(request, response) {
-    //console.log('TEST');
-    //response.render('index');
-    pool.query('SELECT * FROM recipes', (err, result) => {
-    if (err) {
-      return next(err);
-    }
-    //console.log(err, res);
-    response.render('index', {recipes: result.rows});
-    //res.send(res.rows[0]);
-    
-    //pool.end();
-  })
-})
+    pool.connect(function(err, client, done)) {
+        if(err) {
+            return console.error('error fetching client from pool', err);
+        }
+        client.query('SELECT * FROM recipes', function(err, result) {
+            //call 'done()' to release the client back to the pool
+            if(err) {
+                return console.error('error running query', err);
+            }
+            res.render('index', {recipes: result.rows})
+            done();
+        });
+    });
+});
+
+// //	pg.connect(connect, function(err, client, done) {
+//         if(err) {
+//             return console.error('error fetching client from pool', err);
+//         }
+//         client.query('SELECT * FROM recipes', function(err, result) {
+//             //call 'done()' to release the client back to the pool
+//             if(err) {
+//                 return console.error('error running query', err);
+//             }
+//             res.render('index', {recipes: result.rows})
+//             done();
+//         });
+//     });
+// });
+
 // Server
 app.listen(3000, function(){
 	console.log('Server Started on Port 3000');
